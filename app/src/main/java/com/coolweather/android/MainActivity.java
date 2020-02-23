2745e7f2b0d33b8aa346b3930fdfbaf2897ecf15
Snake <261667318@qq.com>
2020-02-23 19:26:10 +0800

    add weather show function

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -1,8 +1,12 @@
 package com.coolweather.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import pl.com.salsoft.sqlitestudioremote.SQLiteStudioService;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SQLiteStudioService.instance().start(this);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if(prefs.getString("weather", null) != null){
+            Intent intent = new Intent(this,WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SQLiteStudioService.instance().stop();
     }
 }
---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,16 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity{
+        public String aqi;
+
+        public String pm25;
+
+        @SerializedName("qlty")
+        public String quality;
+    }
+}
